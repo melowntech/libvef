a0b22cd70d8108136b674f2f479c82e1328f2c00
Vaclav Blazek <vaclav.blazek@gmail.com>
2020-07-17 13:40:02 +0200

    opencv-3/4 compatibility

--- vef/tilecutter.cpp
+++ vef/tilecutter.cpp
@@ -32,6 +32,8 @@
 #include "math/transform.hpp"
 #include "math/extent.hpp"
 
+#include "imgproc/cvcompat.hpp"
+
 #include "vts-libs/vts/meshop.hpp"
 #include "vts-libs/tools-support/repackatlas.hpp"
 
@@ -189,7 +191,7 @@ cv::Mat Cutter::loadTexture(const fs::path &path) const
     }
 
     auto is(archive.istream(path));
-    auto tex(cv::imdecode(is->read(), CV_LOAD_IMAGE_COLOR));
+    auto tex(cv::imdecode(is->read(), IMGPROC_IMREAD(COLOR)));
 
     if (!tex.data) {
         LOGTHROW(err2, std::runtime_error)
