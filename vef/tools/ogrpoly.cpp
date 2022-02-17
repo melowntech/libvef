0bc412488c88ea8707d6983144d80084cc48de08
Vaclav Blazek <vaclav.blazek@citationtech.net>
2022-02-17 11:07:16 +0100

    vef2vef: fixed extents polygon orientation

--- vef/tools/ogrpoly.cpp
+++ vef/tools/ogrpoly.cpp
@@ -181,9 +181,9 @@ Polygons polygonsFromExtents(const math::Extents2 &extents)
     auto &polygon(polygons.back());
 
     polygon.push_back(ll(extents));
-    polygon.push_back(ul(extents));
-    polygon.push_back(ur(extents));
     polygon.push_back(lr(extents));
+    polygon.push_back(ur(extents));
+    polygon.push_back(ul(extents));
     polygon.push_back(ll(extents));
 
     return polygons;
@@ -195,4 +195,3 @@ polygonsFromExtents(const boost::optional<math::Extents2> &extents)
     if (extents) { return polygonsFromExtents(*extents); }
     return boost::none;
 }
-
